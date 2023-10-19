921893b1737271ffced6ca65db1c7c2a39e8cd3d
Bhinsar <bhinsarjagdish2003@gamil.com>
2023-10-19 20:35:07 +0530

    awtawt

--- thrid/script.js
+++ thrid/script.js
@@ -1,8 +1,10 @@
-let songItem = 0;
+let songIndex = 1;
 let audioElement = new Audio('photes/songs/1.mp3');
 let masterPlay = document.getElementById('play');
 let myProgreesBar = document.getElementById('myprogressbar')
 let gif = document.getElementById('gif')
+let songItems = Array.from(document.getElementsByClassName('songItem'))
+
 
 let song = [
     {songName:"Warriyo - Mortals [NCS Release)",filePath:"photes/songs/1.mp3",coverPath:"photes/covers/1.jpg"},
@@ -11,7 +13,7 @@ let song = [
     {songName:"Different Heaven & EH!DE - My Heart [NCS Release]",filePath:"photes/songs/4.mp3",coverPath:"photes/covers/4.jpg"},
     {songName:"Janji-Heroes-Tonight-feat-Johnning-NCS-Release",filePath:"photoes/songs/5.mp3",coverPath:"photes/covers/5.jpg"},
     {songName:"Rabba - Salam-e-lshq",filePath:"photes/songs/6.mp3",coverPath:"photes/covers/6.jpg"},
-    {songName:"Sakhiyaan - Salam-e-lshq",filePath:"photes/songs/7.mp3",coverPath:"phtoes/covers/7.jpg"},
+    {songName:"Sakhiyaan - Salam-e-lshq",filePath:"photes/songs/7.mp3",coverPath:"photes/covers/7.jpg"},
     {songName:"Bhula Dena - Salam-e-lshq",filePath:"photes/songs/8.mp3",coverPath:"photes/covers/8.jpg"},
     {songName:"Tumhari Kasam - Salam-e-lshq",filePath:"photes/songs/9.mp3",coverPath:"photes/covers/9.jpg"},
     {songName:"Na Jaana - Salam-e-lshq",filePath:"photes/songs/10.mp3",coverPath:"photes/covers/10.jpg"}
@@ -31,4 +33,56 @@ masterPlay.addEventListener('click',()=>{
 audioElement.addEventListener('timeupdate',()=>{
     let progress = parseInt((audioElement.currentTime/audioElement.duration)*100);
     myProgreesBar.value = progress;
+})
+myProgreesBar.addEventListener('change',()=>{
+    audioElement.currentTime = (myProgreesBar.value*audioElement.duration)/100;
+})
+songItems.forEach((element ,i )=> {
+    element.getElementsByTagName('img')[0].src=song[i].coverPath;
+    element.getElementsByClassName('songName')[0].innertext=song[i].songName;
+});
+const makeAllPlay = ()=>{
+    Array.from(document.getElementsByClassName('songitemplay')).forEach((element)=>{
+        element.innerHTML='play_circle'
+    })
+}
+Array.from(document.getElementsByClassName('songitemplay')).forEach((element) => {
+    element.addEventListener('click', (e) => {
+        makeAllPlay();
+        songIndex = parseInt(e.target.id);
+        audioElement.src = `photes/songs/${songIndex}.mp3`;
+        audioElement.currentTime = 0;
+        audioElement.play();
+        e.target.innerHTML = 'pause_circle';
+        masterPlay.innerHTML = 'pause_circle';
+        gif.style.opacity = 1;
+    });
+  });
+document.getElementById('next').addEventListener('click',()=>{
+    if(songIndex>=9){
+        songIndex=0
+    }
+    else{
+        songIndex+=1;
+    }
+    audioElement.src = `photes/songs/${songIndex}.mp3`;
+    audioElement.currentTime = 0;
+    audioElement.play();
+    e.target.innerHTML = 'pause_circle';
+    masterPlay.innerHTML = 'pause_circle';
+    gif.style.opacity = 1;
+})
+document.getElementById('previous').addEventListener('click',()=>{
+    if(songIndex<=0){
+        songIndex=0
+    }
+    else{
+        songIndex-=1;
+    }
+    audioElement.src = `photes/songs/${songIndex}.mp3`;
+    audioElement.currentTime = 0;
+    audioElement.play();
+    e.target.innerHTML = 'pause_circle';
+    masterPlay.innerHTML = 'pause_circle';
+    gif.style.opacity = 1;
 })
